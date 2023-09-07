279cc649385083af7da418fd57f806f1d95090e3
Guido Bergman <gbergman@fi.uba.ar>
2023-09-06 23:44:55 -0300

    Mejoro el healthcheck

--- health-check/health-check.sh
+++ health-check/health-check.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-MESAGE_TO_SEND="HEALTCHECK"
 
 . config-file
 
 
 while true
 do
-  response=$(echo $MESAGE_TO_SEND | nc -w 5 $SERVER_IP $SERVER_PORT)
+  response=$(echo $MESAGE_TO_SEND | nc -w 5 $SERVER_IP $SERVER_PORT  2> /dev/null)
   timestamp=$(date +"%F %T")
-  if [[ $response == $MESAGE_TO_SEND ]]
+  if [[ "$response" == $MESAGE_TO_SEND ]]
   then
     echo "$timestamp INFO     action: health-check | result: OK"
   else
